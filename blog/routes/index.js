24bb1bb545d2435f235cc2c4a24f4c833f153987
Yang Yang Spurs  (NCS) <spursy@ncs.com.sg>
2016-10-25 17:24:51 +0800

    add tag module and PV count modules.

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -130,8 +130,11 @@ module.exports = function(app) {
 
   app.post('/post', checkLogin);
   app.post('/post', function (req, res) {
+    // var currentUser = req.session.user,
+    //   post = new Post(currentUser.name, req.body.title, req.body.post);
     var currentUser = req.session.user,
-      post = new Post(currentUser.name, req.body.title, req.body.post);
+      tags = [req.body.tag1, req.body.tag2, req.body.tag3],
+      post = new Post(currentUser.name, req.body.title, tags, req.body.post);
     post.save(function (err) {
       if (err) {
         req.flash('error', err); 
@@ -195,6 +198,37 @@ module.exports = function(app) {
     });
   });
 
+  app.get('/tags', function (req, res) {
+    Post.getTags(function (err, posts) {
+      if (err) {
+        req.flash('error', err); 
+        return res.redirect('/');
+      }
+      res.render('tags', {
+        title: '标签',
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
+
+  app.get('/tags/:tag', function (req, res) {
+    Post.getTag(req.params.tag, function (err, posts) {
+      if (err) {
+        req.flash('error',err); 
+        return res.redirect('/');
+      }
+      res.render('tag', {
+        title: 'TAG:' + req.params.tag,
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
   // app.get('/u/:name', function (req, res) {
   //   //检查用户是否存在
   //   User.get(req.params.name, function (err, user) {
